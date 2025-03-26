8c313484d277e38c6781569f8d4d0e36a1c7372c
salvo1992 <145794449+salvo1992@users.noreply.github.com>
2025-03-26 21:33:54 +0100

    11

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import  { useEffect, useState } from "react"
 import ButtonCV from "../Components/ButtonCV"
 import AOS from "aos"
 import "aos/dist/aos.css"
